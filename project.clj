84a11cb0bd349c2ef810f78b08be639a172c7bac
Christian Betz <christian.betz@performance-media.de>
2016-01-04 12:27:43 +0100

    cleaned dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject org.tinnitus.dl/elastic-riemann "0.1.1-SNAPSHOT"
   :description "A plugin for using ElasticSearch and Kibana with Riemann"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.cli "0.2.1"]
-                 [cheshire "5.3.1"]
                  [clj-logging-config "1.9.6"]
-                 [riemann "0.2.10" :exclusions [joda-time org.slf4j/slf4j-log4j12]]
                  [clojurewerkz/elastisch "2.2.0"]]
-
-
-  :main elastic-riemann.core)
+  :profiles {:provided
+             {:dependencies
+              [[riemann "0.2.10" :exclusions [joda-time org.slf4j/slf4j-log4j12]]]}})
+  
