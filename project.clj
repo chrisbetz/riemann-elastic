fc3d02bca8c3ea4d3d378a3b4b4fc04040ba4752
Christian Betz <christian.betz@performance-media.de>
2016-01-06 13:06:26 +0100

    excludes shadowed (older) dependencies from riemann, which might be the root cause of some strange behavior.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A plugin for using ElasticSearch 2.x and Kibana with Riemann 0.2.10"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [clojurewerkz/elastisch "2.2.0"]]
+                 [clojurewerkz/elastisch "2.2.0"  :exclusions [[clj-http] [cheshire]]]]
   :profiles {:provided
              {:dependencies
               [[riemann "0.2.10"]]}})
